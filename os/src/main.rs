88adf12329dcceeef911b1115addb3d52f3bb7c9
ShadowWalker1314 <1761106187@qq.com>
2021-11-14 13:17:10 +0800

    experiment3

--- os/src/main.rs
+++ os/src/main.rs
@@ -41,4 +41,39 @@ pub fn rust_main() -> ! {
     println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     println!("Hello, world!");
     panic!("Shutdown machine!");
+
+}
+
+#![no_std]
+#![no_main]
+#![feature(asm)]
+#![feature(global_asm)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod syscall;
+mod trap;
+mod batch;
+
+global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("[Kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
